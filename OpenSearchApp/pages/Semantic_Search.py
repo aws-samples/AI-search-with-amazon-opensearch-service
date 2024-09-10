d30fa36bfb3604c4b87d58e4482612daca309b59
Ubuntu <ubuntu@ip-172-31-67-112.ec2.internal>
2024-09-10 09:19:23 +0000

    chanes in new branch

--- OpenSearchApp/pages/Semantic_Search.py
+++ OpenSearchApp/pages/Semantic_Search.py
@@ -722,12 +722,12 @@ with col3:
     st.number_input("No. of docs", min_value=1, max_value=50, value=5, step=5,  key='input_K', help=None)
 with col4:
     st.markdown("<p style='fontSize:14.5px'>Evaluate</p>",unsafe_allow_html=True)
-    evaluate = st.toggle(' ', key = 'evaluate', disabled = False) #help = "Checking this box will use LLM to evaluate results as relevant and irrelevant. \n\n This option increases the latency")
-    if(evaluate):
-        st.session_state.input_evaluate = "enabled"
-        #llm_eval.eval(st.session_state.questions, st.session_state.answers)
-    else:
-        st.session_state.input_evaluate = "disabled"
+    # evaluate = st.toggle(' ', key = 'evaluate', disabled = False) #help = "Checking this box will use LLM to evaluate results as relevant and irrelevant. \n\n This option increases the latency")
+    # if(evaluate):
+    #     st.session_state.input_evaluate = "enabled"
+    #     #llm_eval.eval(st.session_state.questions, st.session_state.answers)
+    # else:
+    #     st.session_state.input_evaluate = "disabled"
         
 
 if(search_all_type == True or 1==1):
